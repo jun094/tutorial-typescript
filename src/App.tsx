9055d4b8975d85af13a180d7e6f8018a599d29ec
jun-young <jun094@naver.com>
2021-07-24 15:12:42 +0900

    props로 함수 전달하는 방법

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import Hello from './Hello';
+import MyForm from './MyForm';
 
 function App() {
     const onClick = (name: string) => {
         console.log(`${name} says hello`);
     };
-    return <Hello name="jun" onClick={onClick} />;
+    const onSubmit = (form: { name: string; description: string }) => {
+        console.log(form);
+    };
+    return (
+        <>
+            <Hello name="jun" onClick={onClick} />
+            <MyForm onSubmit={onSubmit} />
+        </>
+    );
 }
 
 export default App;
